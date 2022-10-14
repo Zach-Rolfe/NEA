db39f43b0441aa4cd471adf34f7938534e4ca372
Zach-Rolfe <zacharyrolfe@exe-coll.ac.uk>
2022-10-14 21:24:05 +0100

    V1 Commit

--- Program.cs
+++ Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create a new instance of the class.
+            var myClass = new MyClass();
+
+            // Call the method.
+            myClass.MyMethod();
+
+        }
+    }
+}
