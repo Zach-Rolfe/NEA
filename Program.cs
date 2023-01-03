178edbea7fa4da2f56960388ed931cf5bfef4693
Zach-Rolfe <zacharyrolfe_exe-coll.ac.uk>
2023-01-03 18:28:24 +0000

    V1 of it

--- Program.cs
+++ Program.cs
@@ -1,2 +1,12 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+//create a login system
+//create a register system
+//create a profile system
+
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
